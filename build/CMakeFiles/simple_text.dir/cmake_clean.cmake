f16a83a93a6d8059d6bf1c1b6275ce6bfb205983
KailyhSF <91802555+kailyhpotatoestew@users.noreply.github.com>
2026-07-18 23:27:16 -0400

    Add files via upload

---
+++ build/CMakeFiles/simple_text.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_text.dir/link.d"
+  "CMakeFiles/simple_text.dir/main.c.o"
+  "CMakeFiles/simple_text.dir/main.c.o.d"
+  "simple_text"
+  "simple_text.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simple_text.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
